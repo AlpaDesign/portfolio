578c5c5e6a43bc7987c99c69b966e8b4d9626351
AlpaDesign <83193403+AlpaDesign@users.noreply.github.com>
2025-07-17 17:36:31 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,8 +7,169 @@
 	var n=0;
     var httpRequest;
     var files = "files/ppt.txt";
+
+    var skill = [
+        {
+            title: "HTML",
+            Contents: [
+                "HTML5 DTD 문법 준수",
+                "웹 접근성을 높인 Semantic Page 작성",
+                "IR 기법을 이용한 스타일 작성",
+                "반응형 웹 페이지 스타일 작성)",
+                "모바일 페이지 스타일 작성"
+            ],
+            Percent : 99
+        },
+        {
+            title: "CSS",
+            Contents: [
+                "HTML5 DTD 문법 준수",
+                "웹 접근성을 높인 Semantic Page 작성",
+                "IR 기법을 이용한 스타일 작성", ,
+                "화려한 CSS3 Animation 구현",
+                "반응형 웹 페이지 스타일 작성",
+                "모바일 페이지 스타일 작성",
+                "CSS 전처리 기능 프로그래밍(SASS)"
+            ],
+            Percent: 80
+        },
+        {
+            title: "JS",
+            Contents: [
+                "React 및 TypeScript 사용가능",
+                "Next js 를 이용한 프로젝트 활용 가능",
+                "JavaScript 플러그인 활용 및 구현(jQuery)",
+                "Parallax 웹 페이지 구현",
+                "외부 API 구현(Google Map, Carousel)",
+                "EcmaScript 6.0 기반 Native JavaScript 활용",
+                "JAX를 이용한 데이터 응용",
+                "모바일 페이지 스타일 작성",
+            ],
+            Percent: 90
+        },
+        {
+            title: "React",
+            Contents: [
+                "React 및 TypeScript 사용가능",
+                "Next js 를 이용한 프로젝트 활용 가능",
+                "JavaScript 플러그인 활용 및 구현(jQuery)",
+                "Parallax 웹 페이지 구현",
+                "외부 API 구현(Google Map, Carousel)",
+                "EcmaScript 6.0 기반 Native JavaScript 활용",
+                "JAX를 이용한 데이터 응용",
+                "모바일 페이지 스타일 작성",
+            ],
+            Percent: 80
+        },
+        {
+
+            title: "JSP",
+            Contents: [
+                "Scriptlet(스크립트릿)을 이용한 java sever page 활용",
+                "jsp로만 활용한 페이지 구성(MVC model1)",
+                "Expression(표현식)을 활용하여 HTML 표시",
+            ],
+            Percent: 60
+        },
+        {
+            title: "JAVA",
+            Contents: [
+                "JAVA 언어를 활용한 기본 응용소프트웨어 구현 능력 함양",
+                "JAVA의 알고리즘 활용",
+                "스프링 프레임워크, 이클립스, 아파치 개발 도구 활용",
+                "SQL, 오라클을 기반으로 한 서버 구축 및 데이터 입출력 체크",
+                "DataBase 기반으로 게시판 제작"
+            ],
+            Percent: 70
+        },
+        {
+            title: "PHP",
+            Contents: [
+                "php 언어를 활용한 기본 응용소프트웨어 구현 능력 함양",
+                "php로 간단한 쇼핑몰 제작가능",
+                "아파치 서버 활용"
+            ],
+            Percent: 80
+        },
+        {
+
+            title: "Oracle",
+            Contents: [
+                "DataBase 서버 구현과 API를 기반으로 하는 프로그램 개발 및 데이터 개발",
+                "SQL, 오라클을 기반으로 한 서버 구축 및 데이터 입출력 체크",
+                "DataBase 기반으로 정규화된 프로젝트 설계 가능",
+                "DataBase 개념적 물리적 논리적설계 가능"
+            ],
+            Percent: 60
+        },
+        {
+
+            title: "MYSQL",
+            Contents: [
+                "DataBase 서버 구현과 API를 기반으로 하는 프로그램 개발 및 데이터 개발",
+                "SQL, 오라클을 기반으로 한 서버 구축 및 데이터 입출력 체크",
+                "DataBase 기반으로 정규화된 프로젝트 설계 가능",
+                "DataBase 개념적 물리적 논리적설계 가능"
+            ],
+            Percent: 70
+        },
+        {
+            title: "Spring",
+            Contents: [
+                "Spring Boot를 활용",
+                "MVC mode2를 활용한 페이지 구성",
+                "JAVA 언어를 활용한 기본 응용소프트웨어 구현 능력 함양",
+                "JAVA의 알고리즘 활용",
+                "스프링 프레임워크, 이클립스, 아파치 개발 도구 활용",
+                "SQL, 오라클을 기반으로 한 서버 구축 및 데이터 입출력 체크",
+                "DataBase 기반으로 게시판 제작"
+            ],
+            Percent: 80
+        },
+        {
+            title: "C#",
+            Contents: [
+                "WPF로 window 프로그램 제작 가능",
+                ".NET FrameWork WindowsForm을 활용한 프로그램 제작 가능",
+                "아두이노 코드 설계 프로그램 제작",
+                "윈도우 스케줄러 관리 프로그램 제작",
+                "그외 다수 라이브러리 패키지 제작"
+            ],
+            Percent: 80
+        },
+        {
+            title: "C++",
+            Contents: [
+                "MFC 프로젝트 활용 가능",
+                "POS 단말 프로그램 제작"
+            ],
+            Percent: 40
+        },
+        {
+            title: "Python",
+            Contents: [
+                "간단한 스크립트 작성하여 빌드가능",
+                "파이썬을 활용한 통신 프로그램 제작"
+            ],
+            Percent: 50
+        },
+    ]
+
     
-    var to=0;
+    var to = 0;
+
+
+    var load = (function () {
+
+        var elem = $("#skillChart");
+        for (let i = 0; i < skill.length; i++) {
+            var html = `
+                <div class="circlechart" data-percentage="${skill[i].Percent}">${skill[i].title}</div>
+            `;
+            elem.append(html);
+        }
+    })();
+
 
 	$(window).on("scroll", drawProgressHandler);
 	function drawProgressHandler(){
@@ -206,212 +367,32 @@
 
 
 
-    $(".circlechart").click(function(){
-        var txt = $(this).find(".circle-chart__subline").text();
-        if(txt == "HTML"){
-            $("#modal_tbody").html(htmlText());
-        } else if(txt == "CSS"){
-            $("#modal_tbody").html(cssText());
-        } else if(txt == "JS"){
-            $("#modal_tbody").html(jsText());
-        } else if(txt == "JSP"){
-            $("#modal_tbody").html(jspText());
-        } else if(txt == "JAVA"){
-            $("#modal_tbody").html(javaText());
-        } else if(txt == "PHP"){
-            $("#modal_tbody").html(phpText());
-        } else if(txt == "Oracle"){
-            $("#modal_tbody").html(oracleText());
-        } else if(txt == "Spring"){
-            $("#modal_tbody").html(springText());
-        } else {
-            $("#modal_tbody").html("");
+    $(".circlechart").click(function () {
+
+        const idx = $(".circlechart").index(this);
+
+        for (let i = 0; i < skill.length; i++) {
+            if (idx != i) continue;
+            $("#modal_tbody").html(SkillContents(skill[i].Contents));
+            $(".modal h3").text(skill[i].title);
         }
-        $(".modal h3").text(txt);
+        
         $(".dim").addClass("on");
         $(".modal").addClass("on");
     });
 
+    var SkillContents = (list) => {
+        var html = "";
+        for (let i = 0; i < list.length; i++) {
+            html += `<tr>
+                <td>${list[i]}</td>
+            <tr>`
+        }
+
+        return html;
+    }
+
     
-
-    function htmlText(){
-        var text = `
-                    <tr>
-                        <td>HTML5 DTD 문법 준수</td>
-                    </tr>
-                    <tr>
-                        <td>웹 접근성을 높인 Semantic Page 작성</td>
-                    </tr>
-                    <tr>
-                        <td>IR 기법을 이용한 스타일 작성</td>
-                    </tr>
-                    <tr>
-                        <td>반응형 웹 페이지 스타일 작성)</td>
-                    </tr>
-                    <tr>
-                        <td>모바일 페이지 스타일 작성</td>
-                    </tr>
-                    `;
-
-        return text;
-    }
-
-    function cssText(){
-        var text = `
-                    <tr>
-                        <td>HTML5 DTD 문법 준수</td>
-                    </tr>
-                    <tr>
-                        <td>웹 접근성을 높인 Semantic Page 작성</td>
-                    </tr>
-                    <tr>
-                        <td>IR 기법을 이용한 스타일 작성</td>
-                    </tr>
-                    <tr>
-                        <td>화려한 CSS3 Animation 구현</td>
-                    </tr>
-                    <tr>
-                        <td>반응형 웹 페이지 스타일 작성)</td>
-                    </tr>
-                    <tr>
-                        <td>모바일 페이지 스타일 작성</td>
-                    </tr>
-                    <tr>
-                        <td>CSS 전처리 기능 프로그래밍(SASS)</td>
-                    </tr>
-                    `;
-
-        return text;
-    }
-
-    function jsText(){
-        var text = `
-                    <tr>
-                        <td>JavaScript 플러그인 활용 및 구현(jQuery)</td>
-                    </tr>
-                    <tr>
-                        <td>Parallax 웹 페이지 구현</td>
-                    </tr>
-                    <tr>
-                        <td>외부 API 구현(Google Map, Carousel)</td>
-                    </tr>
-                    <tr>
-                        <td>EcmaScript 6.0 기반 Native JavaScript 활용</td>
-                    </tr>
-                    <tr>
-                        <td>AJAX를 이용한 데이터 응용</td>
-                    </tr>
-                    <tr>
-                        <td>모바일 페이지 스타일 작성</td>
-                    </tr>
-                    <tr>
-                        <td>CSS 전처리 기능 프로그래밍(SASS)</td>
-                    </tr>
-                    <tr>
-                        <td>라이브러리 직접 제작</td>
-                    </tr>
-                    `;
-
-        return text;
-    }
-
-    function jspText(){
-        var text = `
-                    <tr>
-                        <td>Scriptlet(스크립트릿)을 이용한 java sever page 활용</td>
-                    </tr>
-                    <tr>
-                        <td>jsp로만 활용한 페이지 구성(MVC model1)</td>
-                    </tr>
-                    <tr>
-                        <td>Expression(표현식)을 활용하여 HTML 표시</td>
-                    </tr>
-                    `;
-
-        return text;
-    }
-    function javaText(){
-        var text = `
-                    <tr>
-                        <td>JAVA 언어를 활용한 기본 응용소프트웨어 구현 능력 함양</td>
-                    </tr>
-                    <tr>
-                        <td>JAVA의 알고리즘 활용</td>
-                    </tr>
-                    <tr>
-                        <td>스프링 프레임워크, 이클립스, 아파치 개발 도구 활용</td>
-                    </tr>
-                    <tr>
-                        <td>SQL, 오라클을 기반으로 한 서버 구축 및 데이터 입출력 체크</td>
-                    </tr>
-                    <tr>
-                        <td>DataBase 기반으로 게시판 제작</td>
-                    </tr>
-                    `;
-
-        return text;
-    }
-    function phpText(){
-        var text = `
-                    <tr>
-                        <td>php 언어를 활용한 기본 응용소프트웨어 구현 능력 함양</td>
-                    </tr>
-                    <tr>
-                        <td>php로 간단한 쇼핑몰 제작가능</td>
-                    </tr>
-                    <tr>
-                        <td>아파치 서버 활용</td>
-                    </tr>
-                    `;
-
-        return text;
-    }
-    function oracleText(){
-        var text = `
-                    <tr>
-                        <td>DataBase 서버 구현과 API를 기반으로 하는 프로그램 개발 및 데이터 개발</td>
-                    </tr>
-                    <tr>
-                        <td>SQL, 오라클을 기반으로 한 서버 구축 및 데이터 입출력 체크</td>
-                    </tr>
-                    <tr>
-                        <td>DataBase 기반으로 게시판 제작</td>
-                    </tr>
-                    <tr>
-                        <td>DataBase 개념적 물리적 논리적설계 가능</td>
-                    </tr>
-                    
-                    `;
-
-        return text;
-    }
-    function springText(){
-        var text = `
-                    <tr>
-                        <td>Spring Boot를 활용</td>
-                    </tr>
-                    <tr>
-                        <td>MVC mode2를 활용한 페이지 구성</td>
-                    </tr>
-                    <tr>
-                        <td>JAVA 언어를 활용한 기본 응용소프트웨어 구현 능력 함양</td>
-                    </tr>
-                    <tr>
-                        <td>JAVA의 알고리즘 활용</td>
-                    </tr>
-                    <tr>
-                        <td>스프링 프레임워크, 이클립스, 아파치 개발 도구 활용</td>
-                    </tr>
-                    <tr>
-                        <td>SQL, 오라클을 기반으로 한 서버 구축 및 데이터 입출력 체크</td>
-                    </tr>
-                    <tr>
-                        <td>DataBase 기반으로 게시판 제작</td>
-                    </tr>
-                    `;
-
-        return text;
-    }
    
     $("#page3 .content li dt").mouseenter(function(){
         
